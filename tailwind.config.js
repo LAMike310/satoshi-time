e4ecdc7db8ba6766efda8dfe31edf97fe578312f
Mikey Kones <mikeykones@Mikeys-MacBook-Air.local>
2022-05-06 11:39:47 -0700

    Added purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,5 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
-  purge: [ "./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}", ]
+  purge: {enabled: process.env.NODE_ENV === "production", content: [ "./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}", ]}
 }
